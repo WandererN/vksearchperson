8e5d2476478326af71d1a6150f7c6094000d3be4
WandererN <izhdanovn@gmail.com>
2020-02-16 16:37:00 +0300

    initial commit

---
+++ app/src/main/java/com/jh/vksearchperson/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jh.vksearchperson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jh.vksearchperson.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/jh/vksearchperson/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.jh.vksearchperson.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
